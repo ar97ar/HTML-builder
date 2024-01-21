2c17c6ce3efb5ff344c942b3d675ad8935dab160
Andrey <an97ron@gmail.com>
2024-01-22 01:01:45 +0300

    feat: first task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,1 +1,7 @@
-//
+const fs = require('fs');
+const path = require('path');
+
+const pathToFile = path.join(__dirname, 'text.txt');
+const stream = fs.createReadStream(pathToFile, 'utf-8');
+
+stream.on('data', (chunk) =>  console.log(chunk));
